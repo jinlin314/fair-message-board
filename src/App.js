e663150912b0dcee4ba0a4754fd393415ab8b457
Jin Lin <jinlin314@hotmail.com>
2017-12-07 01:09:30 -0500

    react router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getAll } from './action-creators/posts'
+import { getAll } from './action-creators/posts';
+import Posts from './Posts';
+import Post from './Post';
 
 
 class App extends Component {
@@ -16,6 +18,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Posts} />
+            <Route path="/posts/:id" component={Post} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
